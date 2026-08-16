f80802041d83dcdf7b30d7bffcb63b8e5e62aa39
Kökény Bence <bence.kokeny04@gmail.com>
2026-08-16 12:24:42 +0200

    initial state

---
+++ src/app/button-with-icon/button-with-icon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-icon',
+  imports: [],
+  templateUrl: './button-with-icon.html',
+  styleUrl: './button-with-icon.css',
+})
+export class ButtonWithIcon {
+
+}
